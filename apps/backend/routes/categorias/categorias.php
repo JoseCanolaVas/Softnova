50a46f51eea489574a86887871ce036a023b813f
JoseCanolaVas <josevas1347@gmail.com>
2026-09-22 09:16:10 -0500

    categorias

--- apps/backend/routes/categorias/categorias.php
+++ apps/backend/routes/categorias/categorias.php
@@ -7,7 +7,7 @@ Route::prefix('categorias')->group(function () {
     Route::controller(CategoriaController::class)->group(function () {
         Route::get('/listar', 'listarCategorias');
         Route::post('/crear', 'crearCategoria');
-        Route::put('/actualizar', 'actualizarCategoria');
-        Route::delete('/eliminar', 'eliminarCategoria');
+        Route::put('/actualizar/{id}', 'actualizarCategoria');
+        Route::delete('/eliminar/{id}', 'eliminarCategoria');
     });
 });
